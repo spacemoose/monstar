032198848b55925f017197dae6d9e778b1705c60
STG <glen.stark@profidatagroup.com>
2018-02-14 09:02:24 +0100

    removed simple_recorder from detail

---
+++ source/simple_recorder.cpp
@@ -0,0 +1,30 @@
+#include "simple_recorder.hpp"
+#include "detail/graphite_provider.hpp"
+
+namespace monstar {
+
+/// a single recorder is used to record a single value.
+simple_recorder::simple_recorder(std::string metric_name)
+  : m_metric_path(detail::graphite_provider::get_prefix() + "." + metric_name)
+{
+}
+
+simple_recorder::simple_recorder(const simple_recorder& gr)
+  : m_metric_path(gr.m_metric_path)
+{
+}
+
+simple_recorder::~simple_recorder() {}
+
+/// Use this operator to record a value with an automatic timestamp
+void simple_recorder::operator()(double val) { this->operator()(epoch::now(), val); }
+
+/// This is useful for creating fake data, or making sure a set of vals are
+/// recorded with an identical timestamp.
+void simple_recorder::operator()(int secs_since_epoch, double val)
+{
+	m_msg.str("");
+	m_msg << m_metric_path << " " << val << " " << secs_since_epoch << "\n";
+	(*m_provider)(m_msg.str());
+}
+}
--- source/simple_recorder.hpp
+++ source/simple_recorder.hpp
@@ -1,53 +1,40 @@
 #pragma once
 
-#include "detail/graphite_provider.hpp"
 #include "epoch.hpp"
+#include <iostream>
+#include <memory>
 #include <sstream>
 #include <string>
 
-#include <iostream>
-
 namespace monstar {
+
 namespace detail {
+class graphite_provider;
+}
 
-
-/// For storing some numeric informatoin (only to Graphite currently),
+/// For storing some numeric information (only to Graphite currently),
 /// at the current timestamp.
 class simple_recorder
 {
   public:
 	/// a single recorder is used to record a single value.
-	simple_recorder(std::string metric_name)
-	  : m_metric_path(graphite_provider::get_prefix() + "." + metric_name)
-	{
-	}
-
-	simple_recorder(const simple_recorder& gr)
-	  : m_metric_path(gr.m_metric_path)
-	{
-
-	}
+	simple_recorder(std::string metric_name);
+	simple_recorder(const simple_recorder& gr);
+    ~simple_recorder();
 
 	/// Use this operator to record a value with an automatic timestamp
-	void operator()(double val) { this->operator()(epoch::now(), val); }
+	void operator()(double val);
 
 	/// This is useful for creating fake data, or making sure a set of vals are
 	/// recorded with an identical timestamp.
-	void operator()(int secs_since_epoch, double val)
-	{
-		m_msg.str("");
-		m_msg << m_metric_path << " " << val << " " << secs_since_epoch << "\n";
-		m_provider(m_msg.str());
-	}
+	void operator()(int secs_since_epoch, double val);
 
-    /// The path to the metric.  Must contain metric name, rest is up to caller.
+	/// The path to the metric.  Must contain metric name, rest is up to caller.
 	const std::string m_metric_path;
 
   private:
-
 	/// @todo single carbon connection, batch posting.
-    graphite_provider m_provider;
+	std::unique_ptr<detail::graphite_provider> m_provider;
 	std::stringstream m_msg;
 };
-}
 }
